a05e0d2403523502546ba82bc52c6f8d1d76c38c
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-06-04 16:36:25 +0700

    perf: optimize login page - remove heavy external scripts (FontAwesome, analytics, scrollbar plugins)

--- staff/index.php
+++ staff/index.php
@@ -23,17 +23,9 @@ if (isset($_GET['login']) && $_GET['login'] === 'failed') {
     LOG IN
   </title>
   <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-  <!-- Nucleo Icons -->
-  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <!-- Material Icons -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" />
   <!-- CSS Files -->
   <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
-  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
   <?php
   $loginError = isset($_GET['error']) ? htmlspecialchars($_GET['error'], ENT_QUOTES, 'UTF-8') : '';
   echo "<script>var loginError = '$loginError';</script>";
@@ -100,24 +92,11 @@ if (isset($_GET['login']) && $_GET['login'] === 'failed') {
   <!--   Core JS Files   -->
   <script src="assets/js/core/popper.min.js"></script>
   <script src="assets/js/core/bootstrap.min.js"></script>
-  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
   <script>
     if (loginError !== '') {
       alert(loginError);
     }
   </script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <script src="assets/js/material-dashboard.min.js?v=3.1.0"></script>
   <!-- PWA Install Banner -->
   <?php include 'pwa-install.php'; ?>
   <!-- PWA Service Worker Registration -->
