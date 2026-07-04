fbb95999e7fa7ef5a39b477e88c88d50d011ba7e
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-07-04 12:24:31 +0700

    debug: run ls command

--- debug_path.php
+++ debug_path.php
@@ -1,16 +1,6 @@
 <?php
 header('Content-Type: text/plain');
-$dirs = glob('/www/wwwroot/*', GLOB_ONLYDIR);
-foreach ($dirs as $dir) {
-    echo "Scanning Dir: $dir\n";
-    try {
-        $it = new RecursiveDirectoryIterator($dir);
-        foreach(new RecursiveIteratorIterator($it) as $file) {
-            if (basename($file) == 'api_sales_task.php') {
-                echo "  Found: $file (Last modified: " . date("Y-m-d H:i:s", filemtime($file)) . ")\n";
-            }
-        }
-    } catch (Exception $e) {
-        echo "  Error: " . $e->getMessage() . "\n";
-    }
-}
+echo "LS /www/wwwroot:\n";
+echo shell_exec('ls -la /www/wwwroot');
+echo "\nFind api_sales_task.php:\n";
+echo shell_exec('find /www/wwwroot -name "api_sales_task.php"');
